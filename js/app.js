11bffa56d1dfe0d139233c5189b4f2184c1cd4a6
sherrytherese <sherry.therese@gmail.com>
2016-04-08 21:39:07 -0700

    Completed JS

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@
 		correctAnswer: 1, //how do I pair up a value to each answer? ie = Netgear = 1, Apple = 2 etc.
 		questionNumber: 0},
 
-
 		{question: "Which cat leaves the most fish?",
 		choices: ["Fred", "Miss Fortune", "Chairman Meow"],
 		correctAnswer: 2,
@@ -40,42 +39,91 @@
 //Display the Questions
 function displayChoices () {
 	for (var i = 0; i <questions[questionNumber].choices.length; i++){
-	var currentChoices = questions[questionNumber].choices[i];
+		var currentChoices = questions[questionNumber].choices[i];
 	$(".myChoices"+i).text("    "+currentChoices);
 	}
 }
 
 function displayQuestions() {
-	currentQuestion = questions[questionNumber].question;
+	var currentQuestion = questions[questionNumber].question;
 	$(".quizQuestions").text(currentQuestion);
+}
+
+//Count Question Number
+function questionCount() {
+	var questionCount = questions[questionNumber].questionNumber+1;
+	$(".questionNumber").text("Question "+questionCount+" of "+questions.length);
+}
+
+//Display Summary Page
+function summaryPage() {
+	$(".quiz").hide();
+	if(correctAnswers > questions.length/2){
+		$(".summary").text("Congratulations! You got "+correctAnswers+" out of "+questions.length+" questions correct!");
+	}
+	else {
+		$(".summary").text("You got "+correctAnswers+" out of "+questions.length+" questions correct. Better luck next time!");
+	}
 }
 
 /*Document Ready*/
 $(document).ready(function(){
+	questionCount();
 	displayQuestions();
 	displayChoices();
 
 	//Submit Button
 	$(".submitButton").click(function(){
 		
-		//Get user selection
-		var userInput = $('input[name=myChoices]:checked').val();
-		console.log("userInput is "+userInput)
+		//Check if end of Quiz
+		if (questions[questionNumber].questionNumber < questions.length-1){
+		console.log("More Questions to Come");
 
-		//Compare user selection to correct answer
-		if (userInput == questions[questionNumber].correctAnswer) {
-			correctAnswers++ //increase the correctAnswers count
+			//Get user selection
+			var userInput = $('input[name=myChoices]:checked').val();
+			console.log("userInput is "+userInput)
+
+			//Compare user selection to correct answer
+			if (userInput == questions[questionNumber].correctAnswer) {
+				correctAnswers++ //increase the correctAnswers count
+			}
+			console.log("Total correctAnswers are "+correctAnswers);
+			console.log("correctAnswer for this question is "+questions[questionNumber].correctAnswer);
+			//Increase questionNumber if less than # questions
+			//if (questions[questionNumber].questionNumber < questions.length-1){
+			//	questionNumber++;
+			//}
+			//else {
+			//	return questionNumber;
+			//}
+			questionNumber++;
+			console.log("questionNumber is "+questionNumber);
+
+			//Display next Questions
+			$('input[name=myChoices]').attr('checked',false);
+			questionCount();
+			displayQuestions();
+			displayChoices();
 		}
-		console.log("Total correctAnswers are "+correctAnswers);
-		console.log("correctAnswer for this question is "+questions[questionNumber].correctAnswer);
-		//Increase questionNumber
-		questionNumber++;
-		console.log("questionNumber is "+questionNumber);
-		displayQuestions();
-		displayChoices();
-	})
-	
+		else {
+		console.log("End of Quiz");
 
+			//Get user selection
+			var userInput = $('input[name=myChoices]:checked').val();
+			console.log("userInput is "+userInput)
+
+			//Compare user selection to correct answer
+			if (userInput == questions[questionNumber].correctAnswer) {
+				correctAnswers++ //increase the correctAnswers count
+			}
+			console.log("Total correctAnswers are "+correctAnswers);
+			console.log("correctAnswer for this question is "+questions[questionNumber].correctAnswer);
+			console.log("questionNumber is "+questionNumber);
+
+			//Display Summary Page
+			summaryPage();
+		}
+	})
 
 })
 
